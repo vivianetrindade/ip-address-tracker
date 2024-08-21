1d76986ebf1687698c2e7db319c38538478184e0
vivianetrindade <trindade.viviane@gmail.com>
2024-08-21 17:04:34 +0200

    Remove application.properties from version control

--- server/src/main/java/com/example/consuming_api_address_tracker/controllers/GeocodingController.java
+++ server/src/main/java/com/example/consuming_api_address_tracker/controllers/GeocodingController.java
@@ -1,6 +1,7 @@
 package com.example.consuming_api_address_tracker.controllers;
 
 import com.example.consuming_api_address_tracker.dto.GeocodingResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,10 +10,13 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class GeocodingController {
 
+    @Value("${private.google.api.key}")
+    private String googleApiKey;
+
     @RequestMapping("/geocoding")
     @ResponseBody
     private GeocodingResponse getGeocoding() {
-        String uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=";
+        String uri = String.format("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=%s", googleApiKey);
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri, GeocodingResponse.class);
     }
